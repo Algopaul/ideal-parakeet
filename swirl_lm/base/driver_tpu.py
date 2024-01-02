21c966aba470f9b65c1244ccf93545886e0a099f
Algopaul <72927083+Algopaul@users.noreply.github.com>
2024-01-02 14:17:03 -0500

    readd tf function wrapper

--- swirl_lm/base/driver_tpu.py
+++ swirl_lm/base/driver_tpu.py
@@ -121,6 +121,7 @@ def initialize_tpu(
       resolver, experimental_device_assignment=device_assignment)
 
 
+@tf.function
 def _inner_wrapped_value_fn(
     replica_ids: Array,
     value_fn: ValueFn,
